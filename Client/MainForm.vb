a46ee6756966d6737462605fa758967616ae0b68
nbcl-karen <karenm@nist.gov>
2016-05-26 15:06:31 -0400

    first draft of GUI-save

--- Client/MainForm.vb
+++ Client/MainForm.vb
@@ -1,3 +1,16 @@
-﻿Public Class MainForm
+﻿Imports System
+Imports System.Windows.Forms
+Imports System.Drawing
 
+
+
+Public Class MainForm
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Clear button was clicked")
+    End Sub
+
+    Private Sub Button2Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Enroll button was clicked")
+    End Sub
 End Class
