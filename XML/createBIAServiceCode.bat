0b9fafb448be26b709f1aecefe0345e6ecb442b7
nbcl-karen <karenm@nist.gov>
2016-06-02 16:13:14 -0400

    correcting paths

---
+++ XML/createBIAServiceCode.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\svcutil" bias-2.0.wsdl cbeff_ed2.xsd iso_country_codes.xsd /l:VB /ser:DataContractSerializer /importXmlTypes /namespace:*,OASIS.BIAS.V2
+ECHO BIAS vb file successfully created.
+timeout /t 5
