ac0d64cbc0c4f90a80f6badc5c18b2b126baa47c
Paul Lee <paulyoungjinlee@gmail.com>
2016-09-07 16:28:58 -0400

    Newest version of service code
    Add Subject To Gallery:
    Complete, Working
    
    Check Quality:
    Complete, "Working". Doesn't really check quality, just returns a successful quality check. Discussed with Kevin.
    
    Create Subject:
    Complete, Working
    
    Classify Biometric Data
    Complete, "Working". Just returns "Face". Could be used for fingerprints, but classification not necessary for face images.
    
    Delete Biographic Data:
    Complete, working. 
    
    Delete Biometric Data:
    Not implemented yet.
    
    Delete Subject:
    Complete, Working
    
    Delete Subject From Gallery:
    Complete, Working
    
    Enroll:
    Complete-ish, Working. Aggregate function, creating subject -> setting biometric data -> adding subject to galley works.
    Need to implement/test identify subject, setBiometricData, both updates, etc. Also structure for new/old subject.
    
    GetIdentifyResults + GetIdentifySubjectResults + GetVerifyResults:
    Will not implement. Designing for synchronous functions, not asynchronous. These three are not needed in a synchronous imp

---
+++ Service/Gallery.vb
@@ -0,0 +1,69 @@
+﻿Option Strict On
+
+Imports System.Runtime.Serialization
+Imports System.IO
+Imports System.Collections.Generic
+
+Module Gallery
+    Public Subjects As SortedDictionary(Of String, Subject) = Nothing
+
+    Public Sub Load()
+        Subjects = New SortedDictionary(Of String, Subject)
+
+        Dim BaseDirectory As New DirectoryInfo("C:\Users\pyl\Documents\NIST\BIAS Web Service Project\BIAS Tester\Service\MasterDB\Subject Records")
+
+        If Not BaseDirectory.Exists Then
+            BaseDirectory.Create()
+        End If
+
+        Dim Files As FileInfo() = BaseDirectory.GetFiles("????????-????-????-????-????????????.???")
+
+        For Each File As FileInfo In Files
+            Dim ID As String = File.Name.Substring(0, File.Name.LastIndexOf("."))
+            Dim CurrentSubject As Subject = LoadSubject(ID)
+
+            If CurrentSubject IsNot Nothing Then
+                Subjects.Add(ID, CurrentSubject)
+            End If
+        Next File
+    End Sub
+
+    Public Sub Save()
+        For Each ID As String In Subjects.Keys
+            UpdateSubject(ID, Subjects(ID))
+        Next
+    End Sub
+
+    Public Function LoadSubject(ByVal ID As String) As Subject
+        Dim Result As Subject
+
+        Using SubjectFile As FileStream = New IO.FileStream(GetFilename(ID), FileMode.OpenOrCreate, FileAccess.Read)
+            Dim dcs As New DataContractSerializer(GetType(Subject))
+            Result = CType(dcs.ReadObject(SubjectFile), Subject)
+            SubjectFile.Close()
+        End Using
+
+        Return Result
+    End Function
+
+    Public Sub UpdateSubject(ByVal ID As String, ByVal s As Subject)
+        Subjects(ID) = s
+
+        Dim fm As FileMode = FileMode.OpenOrCreate
+
+        'MsgBox(GetFilename(ID))
+
+        Using SubjectFile As FileStream = New FileStream(GetFilename(ID), FileMode.OpenOrCreate, FileAccess.Write)
+            Dim dcs As New DataContractSerializer(GetType(Subject))
+            dcs.WriteObject(SubjectFile, s)
+        End Using
+    End Sub
+
+    Public Sub UpdateSubject(ByVal s As Subject)
+        UpdateSubject(s.GUID, s)
+    End Sub
+
+    Private Function GetFilename(ByVal ID As String) As String
+        Return String.Format("{0}\{1}.txt", "C:\Users\pyl\Documents\NIST\BIAS Web Service Project\BIAS Tester\Service\MasterDB\Subject Records", ID)
+    End Function
+End Module
